cb378e8ab9027b3b3aeb4cf011b9d049382143db
Oivanov <alexivanov00000@gmail.com>
2020-10-11 21:56:10 +0300

    first task

---
+++ task1.py
@@ -0,0 +1,5 @@
+input_digit = int(input('Enter your digit: '))
+if ((input_digit> -15 and input_digit<=12) or (input_digit>14 and input_digit<17) or (input_digit>19)):
+    print ('True')
+else:
+    print ('False')
